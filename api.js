1b7756644c130e315d2dd3be1afe09c213ab9e1d
Sammy65433 <samiamlistening@gmail.com>
2026-06-08 13:35:14 -0400

    SBA 308A JavaScript Web Application 3

--- api.js
+++ api.js
@@ -1,9 +1,19 @@
 // Store your API key as a string here (this file will NOT be pushed to GitHub)
 const API_KEY = "de379885-e05e-479c-a352-9b6e1fd90dee";
 
+
+//Handle API REQUEST  
+
+// async function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+
 export async function getTeams() {
 
+// fetch()
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
+// await
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
     const response = await fetch("https://api.balldontlie.io/v1/teams", {
         headers: {
             // confirm authorization 
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
 
+
+
+// `main.js` - event listener and app logic  - 
+// "Controls what happens when app starts
+
 // import getTeams function from api.js
 import { getTeams } from "./api";
