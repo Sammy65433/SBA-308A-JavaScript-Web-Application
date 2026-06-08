67ad58212ab5732029833d7bc5d2b37d18d61aec
Sammy65433 <samiamlistening@gmail.com>
2026-06-08 14:14:47 -0400

    SBA 308A JavaScript Web Application 4 - Working

--- api.js
+++ api.js
@@ -3,24 +3,31 @@ const API_KEY = "de379885-e05e-479c-a352-9b6e1fd90dee";
 
 
 //Handle API REQUEST  
+// Error Handling 
 
 // async function
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
 
 export async function getTeams() {
 
-// fetch()
-https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-// await
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
-    const response = await fetch("https://api.balldontlie.io/v1/teams", {
-        headers: {
-            // confirm authorization 
-            Authorization: API_KEY
-        }
-    });
+    // Make a request 
+    // fetch()
+    // https: //developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
+        // await
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+        const response = await fetch("https://api.balldontlie.io/v1/teams", {
+            headers: {
+                // confirm authorization 
+                Authorization: API_KEY
+            }
+        });
+    // Check for http errors 
+    if (!response.ok) {
+
+        throw new Error(`Failed to fetch teams ${response.status})`);
+    }
     // convert to JSON
     const data = await response.json();
 
--- main.js
+++ main.js
@@ -6,4 +6,23 @@
 // "Controls what happens when app starts
 
 // import getTeams function from api.js
-import { getTeams } from "./api";
+import { getTeams } from "./api.js";
+
+// async function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+
+// Load teams 
+async function loadTeams() {
+try {
+    // wait for Api request 
+    const teams = await getTeams();
+console.log(teams);
+} catch (e) {
+    console.error(e)
+    // print teams
+    // console.log(teams);
+}
+}
+
+// Run function
+loadTeams();
